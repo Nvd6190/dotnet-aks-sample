830583595c4ea045fd26d52a6da69e4b430cf026
Nvd6190 <41731913+Nvd6190@users.noreply.github.com>
2025-07-31 19:56:04 +0530

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from .NET on AKS!");
+app.MapGet("/", () => "Hello from .NET 6 on AKS!");
 
 app.Run();
